eae29fb15c1eee957fb72f7d44c534bb2987e195
eddy-martinez-dev <eddy.martinez.dev@gmail.com>
2026-09-03 18:49:36 -0400

    feat: agrega NominaViewModel con logica de generacion de nomina

---
+++ NominaApp.WPF/ViewModels/NominaViewModel.cs
@@ -0,0 +1,74 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NominaApp.Core.Entities;
+using NominaApp.WPF.Models;
+using NominaApp.WPF.Services;
+using System.Collections.ObjectModel;
+
+namespace NominaApp.WPF.ViewModels;
+
+public partial class NominaViewModel : ObservableObject
+{
+    private readonly INominaApiService _nominaApiService;
+    private readonly IEmpleadoApiService _empleadoApiService;
+
+    [ObservableProperty]
+    private ObservableCollection<EmpleadoModel> empleados = new();
+
+    [ObservableProperty]
+    private EmpleadoModel? empleadoSeleccionado;
+
+    [ObservableProperty]
+    private DateTime periodo = new(DateTime.Today.Year, DateTime.Today.Month, 1);
+
+    [ObservableProperty]
+    private NominaModel? nominaGenerada;
+
+    [ObservableProperty]
+    private bool estaCargando;
+
+    [ObservableProperty]
+    private string? mensajeError;
+
+    public NominaViewModel(INominaApiService nominaApiService, IEmpleadoApiService empleadoApiService)
+    {
+        _nominaApiService = nominaApiService;
+        _empleadoApiService = empleadoApiService;
+    }
+
+    [RelayCommand]
+    private async Task CargarEmpleadosAsync()
+    {
+        var lista = await _empleadoApiService.GetAllAsync();
+        Empleados = new ObservableCollection<EmpleadoModel>(lista);
+    }
+
+    [RelayCommand]
+    private async Task GenerarNominaAsync()
+    {
+        if (EmpleadoSeleccionado is null)
+        {
+            MensajeError = "Selecciona un empleado primero.";
+            return;
+        }
+
+        EstaCargando = true;
+        MensajeError = null;
+        NominaGenerada = null;
+
+        var request = new GenerarNominaModel
+        {
+            EmpleadoId = EmpleadoSeleccionado.Id,
+            Periodo = Periodo
+        };
+
+        var (esExitoso, nomina, error) = await _nominaApiService.GenerarAsync(request);
+
+        if (esExitoso)
+            NominaGenerada = nomina;
+        else
+            MensajeError = error;
+
+        EstaCargando = false;
+    }
+}
